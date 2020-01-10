cb90b0fc7251baee890e5396e01957005718ce20
Benny Meisels <benny.meisels@gmail.com>
2020-01-10 13:55:27 +0200

    Fixed Epaper initialization

--- frozen/Aramcon_Badge/aramcon_badge/badgeio.py
+++ frozen/Aramcon_Badge/aramcon_badge/badgeio.py
@@ -140,7 +140,7 @@ class Badge:
             import adafruit_il0373
             displayio.release_displays()
             self._display = adafruit_il0373.IL0373(self.display_bus, width=296, height=128, rotation=270,
-                                                   seconds_per_frame=5, busy_pin=board.DISP_BUSY)
+                                                   seconds_per_frame=5, busy_pin=board.DISP_BUSY, swap_rams=True, black_bits_inverted=True)
         return self._display
 
     @property
