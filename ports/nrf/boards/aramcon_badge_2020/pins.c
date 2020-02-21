473753eccc7c80f6ad0804d39b851b0e1074d21d
Benny Meisels <benny.meisels@gmail.com>
2020-02-21 14:49:14 +0100

    Added SPI and I2C to board object

--- ports/nrf/boards/aramcon_badge_2020/pins.c
+++ ports/nrf/boards/aramcon_badge_2020/pins.c
@@ -34,6 +34,9 @@ STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_VIBRATION_MOTOR), MP_ROM_PTR(&pin_P0_17) },
 
     { MP_ROM_QSTR(MP_QSTR_BATTERY_SENSE), MP_ROM_PTR(&pin_P0_30) },
+
+    { MP_ROM_QSTR(MP_QSTR_I2C), MP_ROM_PTR(&board_i2c_obj) },
+    { MP_ROM_QSTR(MP_QSTR_SPI), MP_ROM_PTR(&board_spi_obj) },
 };
 
 MP_DEFINE_CONST_DICT(board_module_globals, board_module_globals_table);
